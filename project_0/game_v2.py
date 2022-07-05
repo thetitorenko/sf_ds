fd364785d0303027b8a14104ffcfc9183ad7da2b
thetitorenko <ntitorenko@gmail.com>
2022-07-05 20:40:59 +0600

    fix func and edit readme.md

--- project_0/game_v2.py
+++ project_0/game_v2.py
@@ -5,49 +5,28 @@
 import numpy as np
 
 
-def random_predict(number: int=1) -> int:
-    """Guessing the number by generating random number
+def random_predict(number:int=np.random.randint(1, 101)) -> int:
+    """Guessing the number and counting attempts
     
     Args:
-        number (int, optional): Predict number from score_game(). 
-        Defaults to 1.
+        number (int, optional): Predict number. 
+        Defaults is random generated number 1-100.
 
     Returns:
         int: Count of attempt(s)
     """    
     count = 0
+    lst_num = list(range(101)) # generating list 0-100
     
     while True:
         count += 1
-        predict_number = np.random.randint(1, 101) # computer trying to guess
-        if number == predict_number:
-            break
-    
+        predict_number = int(np.mean(lst_num)) # select mean number, loop1=50
+        half = round(len(lst_num)/2) # find index of the middle of list
+        if predict_number == number: break
+        elif predict_number < number:
+            lst_num = lst_num[half:]    # cut off the list, loop1=[50:100]
+        lst_num = lst_num[:half]        # cut off the list, loop1=[0:49]
     return count
 
 
-def score_game(random_predict) -> int:
-    """Mean of Guess the Number
-
-    Args:
-        random_predict (_type_): Predict function
-
-    Returns:
-        int: The mean of algorithm
-    """
-    count_ls = [] # list of counts of attempts
-    np.random.seed(1) # fix the seed for generating the same random
-    # generate numbers to array for random_predict()
-    random_array = np.random.randint(1, 101, size=(1000)) 
-    
-    for number in random_array:
-        count_ls.append(random_predict(number))
-        
-    score = int(np.mean(count_ls)) # calculate mean via numpy bi-function
-    print(f'The mean of the Guess the Number algorithm is: {score}')
-    
-    return score
-
-# call function for module
-if __name__ == '__main__':
-    score_game(random_predict)
+print(random_predict())
